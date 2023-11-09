b779f0c3c98a3f91546c72e14f438441ca3bab79
Josh Kim <joshuajk@al.insper.edu.br>
2023-11-09 18:39:51 -0300

    Modifiquei a função das cores

--- Cor.py
+++ Cor.py
@@ -2,9 +2,9 @@ def cor(s,lista):
     saida = ''
     for i in range(len(s)):
         if lista[i] == 0:
-            saida += '\033[0;32m {0}'.format(s[i])
+            saida += '\033[0;32m {0} \033[0;37m'.format(s[i])
         elif lista[i] == 1:
-            saida += '\033[0;33m {0}'.format(s[i])
+            saida += '\033[0;33m {0} \033[0;37m'.format(s[i])
         else:
-            saida += '\033[0;31m {0}'.format(s[i])
+            saida += '\033[0;31m {0} \033[0;37m'.format(s[i])
     return saida
